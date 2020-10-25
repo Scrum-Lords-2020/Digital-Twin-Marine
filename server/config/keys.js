39ed9e6b160558443664053b3000b186745d3ac4
ZackAllen1 <zackattacks1127@gmail.com>
2020-10-24 21:55:39 -0400

    test

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://Gregory:1q1q!Q!Q@dtm-cluster.kqjrb.mongodb.net/DTM-Database?retryWrites=true&w=majority"
-}
-
-module.exports = {
-    mongoURI: "mongodb+srv://Gregory:1q1q!Q!Q@dtm-cluster.kqjrb.mongodb.net/DTM-Database?retryWrites=true&w=majority",
-    secretOrKey: "1q2w!Q@W"
-};
