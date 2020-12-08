1cf0320358339b8259b2da6c7a2b8c12190f2dbd
ZackAllen1 <zackattacks1127@gmail.com>
2020-12-08 17:30:01 -0500

    added failed login message

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import Alert from 'react-bootstrap/Alert'
 import Form from 'react-bootstrap/Form'
 import './Login.css'
 import axios from 'axios'
@@ -13,18 +14,23 @@ class Login extends Component{
         super();
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            showError: false
         };
         this.onSubmit = this.onSubmit.bind(this);
         this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+        this.updateError = this.updateError.bind(this);
     }
 
     handleSuccessfulLogin(user) {
         this.props.onSuccessfulLogin(user);
     }
 
+   
+
     onSubmit(e){
         e.preventDefault();
+        this.updateError();
         // get our form data from state
         const {email, password } = this.state;
         //Make login post request and check for errors
@@ -40,6 +46,10 @@ class Login extends Component{
             this.handleSuccessfulLogin(jwt_decode(token));
         }).catch((error) => {
             console.log(error);
+            this.setState({
+                showError: true
+            });
+            this.forceUpdate();
         });
     }
     //Set states for email and password
@@ -52,14 +62,29 @@ class Login extends Component{
         this.setState({ [e.target.name]: e.target.value });
       }
 
+      updateError(){
+        this.setState({
+            showError: false
+        });
+        this.forceUpdate();
+    }
+      
+
     render(){
+       
         return(
             <Container id="signin-container" style={{marginTop: "10%"}}>
                 {this.props.isLoggedIn &&
                     <Redirect to='/home'/>
                 }
+               
                 <h1>Login</h1>
                 <p>Hello! Log in with your email.</p>
+                {this.state.showError && (
+                    <Alert id="login-alert" variant="danger" onClose={() => this.updateError()} dismissible transition>
+                        <Alert.Heading>Incorrect username or password, please try again</Alert.Heading>
+                    </Alert>
+                )}
                 <Row>
                     <Form onSubmit={this.onSubmit}>
                         <Form.Group id="formEmail" controlID="formEmail">
