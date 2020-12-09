fbfc5a22fec793467d28707faf157c3190f7ef6c
Chris Modrono <cmodrono@ufl.edu>
2020-12-09 15:18:55 -0500

    Stop ignoring build so heroku has access to it.

---
+++ client/build/precache-manifest.ca1d0f0096c4a1ec6d97690311f452e6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb8d70f4adadf80c406df2e6d922633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c87cc6943c7750bd9c07",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0079bed79d0c760b2a81",
+    "url": "/static/css/main.56342093.chunk.css"
+  },
+  {
+    "revision": "c87cc6943c7750bd9c07",
+    "url": "/static/js/2.1d2828b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1d2828b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0079bed79d0c760b2a81",
+    "url": "/static/js/main.0a599e16.chunk.js"
+  },
+  {
+    "revision": "d39f83a5769bcd4ce4ec",
+    "url": "/static/js/runtime-main.e9df7fd0.js"
+  },
+  {
+    "revision": "fb8db5f1d7a604c8fa5a366c8e51f0e6",
+    "url": "/static/media/DTM_Logo.fb8db5f1.webp"
+  },
+  {
+    "revision": "905c77bf2b36d23605a1823e48c469d9",
+    "url": "/static/media/Michroma-Regular.905c77bf.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "b791919b4488e8b5d1e4042679bb7d00",
+    "url": "/static/media/gear-icon.b791919b.svg"
+  },
+  {
+    "revision": "4f0bc6429efbcb10e059d79afe9de744",
+    "url": "/static/media/logout-icon.4f0bc642.svg"
+  },
+  {
+    "revision": "a450cc103b020589bf97577c53c009d7",
+    "url": "/static/media/menu.a450cc10.svg"
+  },
+  {
+    "revision": "4830e5530be73999048994784ed04dac",
+    "url": "/static/media/michroma-v10-latin-regular.4830e553.svg"
+  },
+  {
+    "revision": "50d6262fe3251cab8c7b4f5aee060895",
+    "url": "/static/media/michroma-v10-latin-regular.50d6262f.eot"
+  },
+  {
+    "revision": "92b318e10e827c9086d05147cc8d1ab4",
+    "url": "/static/media/michroma-v10-latin-regular.92b318e1.ttf"
+  },
+  {
+    "revision": "930c08586fbce0626087755ad5a0bd32",
+    "url": "/static/media/michroma-v10-latin-regular.930c0858.woff"
+  },
+  {
+    "revision": "dceb11b3f40a5df93bc52e314e3fc3b3",
+    "url": "/static/media/michroma-v10-latin-regular.dceb11b3.woff2"
+  },
+  {
+    "revision": "7cc9a6852fafdee88633d0c55227730f",
+    "url": "/static/media/profile-user.7cc9a685.svg"
+  }
+]);
