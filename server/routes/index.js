2a2a026bb770ccf40c14344f2aded92c20c714b5
Gregory Carver <Gregory.Carver.7@gmail.com>
2020-12-09 12:46:56 -0500

    -

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+// routes/index.js
+const router = require('express').Router();
+const userRoutes = require('./api');
+const path = require('path');
+
+// API routes
+router.use('/api/users', userRoutes);
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/src/index.html'));
+});
+
+module.exports = router;
