a2be9061583cba56a813e42a72d7b4dd7d6ce952
Gregory Carver <Gregory.Carver.7@gmail.com>
2020-12-09 12:48:31 -0500

    -

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ router.use('/api/users', userRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/src/index.html'));
+	res.sendFile(path.join(__dirname, '../../client/src/index.html'));
 });
 
 module.exports = router;
